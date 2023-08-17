6344c0ee2c26773f5948078a63964e5667ba87fb
Prem N <prem.n@consultadd.com>
2023-08-17 20:12:27 +0530

    solved using effiecient approach

--- 475. Heaters.java
+++ 475. Heaters.java
@@ -1,37 +1,7 @@
 // https://leetcode.com/problems/heaters/description/
 
 class Solution {
-    // public int lower_bound(int arr[],int x){
-    //     int i=0;
-    //     int j=arr.length-1;
-    //     int ans=-1;
-    //     while(i<=j){
-    //         int mid=i+(j-i)/2;
-    //         if(arr[mid]>=x){
-    //             ans=mid;
-    //             j=mid-1;
-    //         }else{
-    //             i=mid+1;
-    //         }
-    //     }
-    //     return ans;
-    // }
-    // public int upper_bound(int arr[],int x){
-    //     int i=0;
-    //     int j=arr.length-1;
-    //     int ans=-1;
-    //     while(i<=j){
-    //         int mid=i+(j-i)/2;
-    //         if(arr[mid]>x){
-    //             ans=mid;
-    //             j=mid-1;
-                
-    //         }else{
-    //             i=mid+1;
-    //         }
-    //     }
-    //     return ans;
-    // }
+    
     public int findRadius(int[] houses, int[] heaters) {
         // Arrays.sort(houses);
         // Arrays.sort(heaters);
@@ -40,6 +10,38 @@ class Solution {
             int dis=Integer.MAX_VALUE;
             for(int he:heaters){
                 dis=Math.min(dis,Math.abs(ho-he));
+            }
+            ans=Math.max(ans,dis);
+        }
+        return ans;
+    }
+}
+
+// efficient approach
+
+class Solution {
+    static int upper_bound(int[] a, int low, int high, int element) {
+        while (low < high) {
+            int middle = low + (high - low) / 2;
+            if (a[middle] > element) {
+                high = middle;
+            } else {
+                low = middle + 1;
+            }
+        }
+        return low;
+    }
+    public int findRadius(int[] houses, int[] heaters) {
+        Arrays.sort(houses);
+        Arrays.sort(heaters);
+        int ans=0;
+        for(int ho:houses){
+            int dis=Integer.MAX_VALUE;
+            int i=upper_bound(heaters,0,heaters.length,ho);
+            if(i<heaters.length)
+            dis=Math.min(dis,Math.abs(ho-heaters[i]));
+            if(i>0){
+                dis=Math.min(dis,Math.abs(ho-heaters[i-1]));
             }
             ans=Math.max(ans,dis);
         }
